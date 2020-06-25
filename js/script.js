73583edaaffe357adeb343cde5bf7c4befeae4a5
Jaroslaw Kubiak <kubiakjarek@outlook.com>
2020-06-25 12:21:35 +0100

    validation form

--- js/script.js
+++ js/script.js
@@ -97,6 +97,98 @@ $(document).scroll(function()
 
 // form validation
 
+const contactForm = document.querySelector('#form');
+const errorBox = document.querySelector('#errors');
+
+const email_reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const text_reg = /^[a-zA-Z0-9]{10,}$/
+
+
+
+function validation(){
+  event.preventDefault();
+
+  const name = document.querySelector('#name');
+  const surname = document.querySelector('#surname');
+  const email = document.querySelector('#email');
+  const text = document.querySelector('#textarea');
+
+  let errors = [];
+
+  if(name.value == '') {
+    errors.push({text: 'first name', el: name});
+  }
+  if(surname.value == '') {
+    errors.push({text: 'second name', el: surname});
+  }
+  if(email.value == '') {
+    errors.push({text: 'email', el: email});
+  } else if(!email_reg.test(email.value)) {
+    errors.push({text: 'email', el: email});
+  }
+  if(text.value == '') {
+    errors.push({text: 'text area', el: textarea});
+  } else if(!text_reg.test(text.value)) {
+    errors.push({text: 'text area', el: textarea});
+  }
+  console.log(errors);
+
+  if (errors.length > 0) {
+    makeError(errors);
+    return false;
+  } else {
+    makeOK()
+    return true
+  }
+}
+
+function makeError(errs) {
+  let statement = 'You have errors with the following fields: ';
+
+  errs.map((er, idx) => {
+    er.el.classList.add('footer__wrap-err--text');
+    if(errs.length === idx+1) {
+      statement += er.text;
+    } else {
+      statement += er.text + ', '
+    }
+  })
+  errs[0].el.focus();
+
+  const message = document.createElement('div');
+  message.classList.add('footer__wrap-err--text');
+  message.innerText = statement;
+
+  message.addEventListener('click', function(){
+    errorBox.removeChild(message);
+  })
+  errorBox.appendChild(message);
+
+  function deleteMessage(){
+    errorBox.removeChild(message);
+  }
+  setTimeout(deleteMessage, 2000);
+}
+
+function makeOK(){
+  let statement = ' no errors';
+  const message = document.createElement('div');
+  message.classList.add('footer__wrap-err--ok');
+  message.innerText = statement;
+
+  message.addEventListener('click', function(){
+    errorBox.removeChild(message);
+  })
+  errorBox.appendChild(message);
+
+  function deleteMessage(){
+    errorBox.removeChild(message);
+  }
+  setTimeout(deleteMessage, 1000);
+}
+
+contactForm.addEventListener('submit', validation);
 window.addEventListener("load", slider);
 
 
